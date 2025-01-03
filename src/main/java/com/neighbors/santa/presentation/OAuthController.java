abecc2ba6cf46f26391c2704aa00c1f927d9d909
imscow11253 <seaweed8759@naver.com>
2025-01-03 15:44:28 +0900

    config : swagger 설정 파일 구성

--- src/main/java/com/neighbors/santa/presentation/OAuthController.java
+++ src/main/java/com/neighbors/santa/presentation/OAuthController.java
@@ -3,6 +3,8 @@ package com.neighbors.santa.presentation;
 import com.neighbors.santa.application.oauth.dto.OAuthLoginResponse;
 import com.neighbors.santa.application.oauth.service.OAuthService;
 import com.neighbors.santa.application.baseResponse.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
+    @Operation(summary = "카카오 로그인 API", description = "카카오톡으로 로그인하기 API입니다.")
     @GetMapping("/oauth/kakao/callback")
     public ResponseEntity<BaseResponse<OAuthLoginResponse>> kakaoLogin(@RequestParam("code") String code) {
         return ResponseEntity.ok()
