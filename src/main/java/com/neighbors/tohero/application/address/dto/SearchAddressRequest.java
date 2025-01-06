4e53b3b6ff70a80afdeaa61e21b196bb0631109c
imscow11253 <seaweed8759@naver.com>
2025-01-06 16:29:09 +0900

    feat(validation) : 관할서 주소 검색 requestDTO validation 로직 추가

--- src/main/java/com/neighbors/tohero/application/address/dto/SearchAddressRequest.java
+++ src/main/java/com/neighbors/tohero/application/address/dto/SearchAddressRequest.java
@@ -1,9 +1,11 @@
 package com.neighbors.tohero.application.address.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record SearchAddressRequest (
         @Length(min = 1, max = 50)
+        @NotBlank
         String searchAddress
 ){
 }
