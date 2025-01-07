2d3056e2ada0ed5497d8a4aa652d0cbedfa9cc4f
imscow11253 <seaweed8759@naver.com>
2025-01-07 15:40:41 +0900

    feat(presentation) : 편지 생성 API 로직 구현

---
+++ src/main/java/com/neighbors/tohero/application/letter/service/LetterService.java
@@ -0,0 +1,15 @@
+package com.neighbors.tohero.application.letter.service;
+
+import com.neighbors.tohero.application.baseResponse.BaseResponse;
+import com.neighbors.tohero.application.letter.dto.CreateLetterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LetterService {
+
+    public BaseResponse createLetter(CreateLetterRequest createLetterRequest) {
+        return null;
+    }
+}
