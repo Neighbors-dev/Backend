5f34a09a75afe949a60869cc4dfc4e51223f3a7b
imscow11253 <seaweed8759@naver.com>
2025-01-07 15:29:49 +0900

    hotfix(presentation) : DTO에 validated 어노테이션 추가

--- src/main/java/com/neighbors/tohero/presentation/controller/AddressController.java
+++ src/main/java/com/neighbors/tohero/presentation/controller/AddressController.java
@@ -6,6 +6,7 @@ import com.neighbors.tohero.application.baseResponse.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class AddressController {
     private final AddressService addressService;
 
     @GetMapping("/address")
-    public ResponseEntity<BaseResponse> searchAddress(@ParameterObject SearchAddressRequest searchAddressRequest) {
+    public ResponseEntity<BaseResponse> searchAddress(@ParameterObject @Validated SearchAddressRequest searchAddressRequest) {
         return ResponseEntity.ok()
                 .body(addressService.searchAddress(searchAddressRequest));
     }
