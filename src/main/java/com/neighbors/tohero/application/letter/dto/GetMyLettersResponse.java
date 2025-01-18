f690ad14b96a0a251829b881c29e20f7ba6f4890
imscow11253 <seaweed8759@naver.com>
2025-01-19 04:40:07 +0900

    feat : 내가 작성한 편지 조회 시 공개 여부 DTO 정보 포함

--- src/main/java/com/neighbors/tohero/application/letter/dto/GetMyLettersResponse.java
+++ src/main/java/com/neighbors/tohero/application/letter/dto/GetMyLettersResponse.java
@@ -2,8 +2,6 @@ package com.neighbors.tohero.application.letter.dto;
 
 import com.neighbors.tohero.domain.domain.mainPage.model.Letter;
 
-import java.text.Format;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -16,7 +14,8 @@ public record GetMyLettersResponse (
             String from,
             String createdAt,
             boolean isOpened,
-            String content
+            String content,
+            boolean isPublic
     ){
         public static MyLetterInfo from(Letter letter){
             return new MyLetterInfo(
@@ -25,7 +24,9 @@ public record GetMyLettersResponse (
                     letter.getWriter(),
                     letter.getCreatedDate().format(DateTimeFormatter.ofPattern("YY.MM.dd")),
                     letter.isOpened(),
-                    letter.getLetterContent());
+                    letter.getLetterContent(),
+                    letter.isPublic()
+            );
         }
     }
 
